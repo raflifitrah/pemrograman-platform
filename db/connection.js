5bd32dcc9cc44e2822d2b32b16d379c0e6743ab4
raflifitrah <raflifitrah0102@gmail.com>
2024-11-28 22:06:29 +0700

    first commit

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'mahasiswa_db'
+});
+
+db.getConnection((err, connection) => {
+
+if (err) {
+console.error('Database connection failed:', err.message);
+} else {
+console.log('Connected to database.');
+connection.release();
+}
+});
+
+module.exports = db;
